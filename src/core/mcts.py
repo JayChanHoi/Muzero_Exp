eb9cf0d9e6933510fc960f95682edac96ebc1403
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-07-19 00:55:57 +0800

    udpate

--- src/core/mcts.py
+++ src/core/mcts.py
@@ -2,8 +2,6 @@ import math
 
 import torch
 
-from .cython_func.mcts_utils import CyphonNode
-
 class MinMaxStats(object):
     """A class that holds the min-max values of the tree."""
 
@@ -70,5 +68,3 @@ class MCTS(object):
             min_max_stats.update(node.value())
 
             value = node.reward + self.config.discount * value
-
-Node = CyphonNode
