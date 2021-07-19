254ffc3ec54ca9c35bfe77da168f55e771928a49
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-07-19 13:53:05 +0800

    update

--- src/core/cython_func/setup.py
+++ src/core/cython_func/setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+# from distutils.extension import Extension
+# from Cython.Distutils import build_ext
 
 setup(
-    name='mcts_core',
+    name='cython_func/mcts_core',
     ext_modules=cythonize("mcts_core.pyx")
 )
