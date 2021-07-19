56afbf4b822b0710c545b7d84abbcd5501f395fa
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-07-19 12:43:18 +0800

    update

--- src/core/train.py
+++ src/core/train.py
@@ -294,7 +294,7 @@ def _train(config, shared_storage, replay_buffer, summary_writer):
     target_model.eval()
 
     # wait for replay buffer to be non-empty
-    while ray.get(replay_buffer.size.remote()) == 0:
+    while ray.get(replay_buffer.size.remote()) == 50:
         pass
 
     for step_count in range(config.training_steps):
