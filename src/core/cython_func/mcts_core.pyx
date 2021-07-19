dcc375e1d47a5c77497d8221936ef4aa96252219
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-07-19 11:17:14 +0800

    update

--- src/core/cython_func/mcts_core.pyx
+++ src/core/cython_func/mcts_core.pyx
@@ -5,7 +5,26 @@ import numpy as np
 
 import torch
 
-from mcts_utils import MinMaxStats
+cdef class MinMaxStats(object):
+    """A class that holds the min-max values of the tree."""
+    cdef public float maximum
+    cdef public float minimum
+
+    def __init__(self, float min_value_bound=None, float max_value_bound=None):
+        self.maximum = min_value_bound if min_value_bound else -float('inf')
+        self.minimum = max_value_bound if max_value_bound else float('inf')
+
+    cpdef void update(self, value float):
+        self.maximum = max(self.maximum, value)
+        self.minimum = min(self.minimum, value)
+        return
+
+    def float normalize(self, value float):
+        if self.maximum > self.minimum:
+            # We normalize only when we have set the maximum and minimum values.
+            return (value - self.minimum) / (self.maximum - self.minimum)
+        else:
+            return value
 
 cdef class CyphonNode(object):
     cdef public int visit_count
