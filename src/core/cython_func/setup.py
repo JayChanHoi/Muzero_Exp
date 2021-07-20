45ba4c70c73036004abca0362c8dfb477cd575ba
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-07-20 15:25:08 +0800

    update

--- src/core/cython_func/setup.py
+++ src/core/cython_func/setup.py
@@ -3,8 +3,8 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 extensions = [
-    Extension("cython_func/mcts_core",["cython_func/mcts_core.pyx"]),
-    Extension("cython_func/mcts",["cython_func/mcts.pyx"])
+    Extension("cython_func/mcts_core",["mcts_core.pyx"]),
+    Extension("cython_func/mcts",["mcts.pyx"])
 ]
 
 setup(
