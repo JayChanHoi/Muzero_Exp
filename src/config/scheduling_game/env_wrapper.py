eb9cd9c44f7abef8c4ec8635f89cca37fce7db0b
Jaychan <jaychan.ai.dev@gmail.com>
2021-07-21 13:35:26 +0800

    update

--- src/config/scheduling_game/env_wrapper.py
+++ src/config/scheduling_game/env_wrapper.py
@@ -20,8 +20,8 @@ class SchedulingGameWrapper(Game):
         self.plant_num, self.truck_num, _ = self.get_obs_info()
 
     def legal_actions(self):
-        # legal_action_list = np.concatenate([np.ones(1), self.env.get_available_actions().flatten().numpy()], axis=0).nonzero()[0].tolist()
-        legal_action_list = [_ for _ in range(self.env.action_space.n)]
+        legal_action_list = np.concatenate([np.ones(1), self.env.get_available_actions().flatten().numpy()], axis=0).nonzero()[0].tolist()
+        # legal_action_list = [_ for _ in range(self.env.action_space.n)]
         return legal_action_list
 
     def get_obs_info(self):
