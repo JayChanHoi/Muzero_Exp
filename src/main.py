98eb1056c178499bd2fdcaf48fdce7171ef77d70
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-07-22 19:28:35 +0800

    update

--- src/main.py
+++ src/main.py
@@ -60,8 +60,8 @@ if __name__ == '__main__':
     # seeding random iterators
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    # muzero_config = ScheudlingControlConfig(os.path.join(os.path.dirname(__file__), 'environment/concrete_scheduling/env_config_real_data.yml'))
-    muzero_config = BitcoinTradeConfig(os.path.join(os.path.dirname(__file__), 'environment/bitcoin/env_config.yml'))
+    muzero_config = ScheudlingControlConfig(os.path.join(os.path.dirname(__file__), 'environment/concrete_scheduling/env_config_real_data.yml'))
+    # muzero_config = BitcoinTradeConfig(os.path.join(os.path.dirname(__file__), 'environment/bitcoin/env_config.yml'))
 
     # set config as per arguments
     exp_path = muzero_config.set_config(args)
